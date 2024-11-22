374e6580dd871a502e2f5b2927a118cc6d062dda
dennysayuwardani <dennysayuwardani@gmail.com>
2024-11-22 10:46:07 +0700

    menambahkan comment

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,29 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart'; // Untuk menampilkan rating bar
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); // Menjalankan aplikasi dengan MyApp
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({super.key}); // Constructor untuk MyApp
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return MaterialApp( //MaterialApp itu seperti "rumah" untuk aplikasi Flutter
+      debugShowCheckedModeBanner: false, //Menonaktifkan banner debug
+      theme: ThemeData( //ThemeData digunakan untuk mengatur tema aplikasi
+        primarySwatch: Colors.blue, //primarySwatch digunakan untuk mengatur warna utama aplikasi
       ),
-      home: Scaffold(
-        appBar: AppBar(
+      home: Scaffold( // Scaffold adalah struktur dasar halaman yang menyediakan AppBar, body, dan layout lainnya
+        appBar: AppBar( // AppBar adalah bagian atas halaman yang menampilkan judul dan tombol navigasi
           title: const Text('Item Product Coffe'),
-          centerTitle: true,
+          centerTitle: true, //centerTitle digunakan untuk mengatur posisi judul di tengah
         ),
-        body: ListView(
+        body: ListView( //ListView adalah widget yang digunakan untuk menampilkan daftar item
           padding: const EdgeInsets.all(16),
-          children: [
-            Center(
+          children: [ //children digunakan untuk menampilkan daftar item
+            Center( 
               child: Center(
                 child: Container(
                   padding: const EdgeInsets.only(top: 5),
@@ -40,7 +40,7 @@ class MyApp extends StatelessWidget {
                           bottomLeft: Radius.circular(100),
                           bottomRight: Radius.circular(100),
                         ),
-                        child: Image.network(
+                        child: Image.network( //Image.network digunakan untuk menampilkan gambar memelalui URL
                           'https://images.pexels.com/photos/7362647/pexels-photo-7362647.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
                           height: 250,
                           width: 200,
@@ -76,7 +76,7 @@ class MyApp extends StatelessWidget {
                                 Text(
                                   'Rp. 45.000',
                                   style: TextStyle(
-                                    decoration: TextDecoration.lineThrough,
+                                    decoration: TextDecoration.lineThrough, //lineThrough digunakan untuk membuat teks ada coretan ditengahnya
                                     color: Colors.brown,
                                     fontSize: 12,
                                   ),
@@ -87,21 +87,21 @@ class MyApp extends StatelessWidget {
                             Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
-                                RatingBar.builder(
-                                  initialRating: 3.5, 
-                                  minRating: 1,
+                                RatingBar.builder( //RatingBar digunakan untuk menampilkan rating bar atau bintang
+                                  initialRating: 3.5, //initialRating digunakan untuk mengatur rating awal
+                                  minRating: 1, //minRating digunakan untuk mengatur rating minimum
                                   direction: Axis.horizontal,
-                                  allowHalfRating: true,
+                                  allowHalfRating: true, //allowHalfRating itu rating yang bisa setengah bintang
                                   itemCount: 5, 
                                   itemSize: 18,
-                                  itemBuilder: (context, _) => Icon(
+                                  itemBuilder: (context, _) => Icon( 
                                     Icons.star,
                                     color: Colors.yellow[600],
                                   ),
-                                  onRatingUpdate: (rating) {
+                                  onRatingUpdate: (rating) { //onRatingUpdate digunakan untuk mengatur rating yang diupdate
                                     print(rating);
                                   },
-                                  ignoreGestures: true,
+                                  ignoreGestures: true, //ignoreGestures digunakan untuk mengabaikan gesture
                                 ),
                                 const SizedBox(
                                   width: 8,
@@ -121,18 +121,18 @@ class MyApp extends StatelessWidget {
                                 print('Shop Now');
                               },
                               style: ButtonStyle(padding:
-                                  WidgetStateProperty.resolveWith((states) {
+                                  WidgetStateProperty.resolveWith((states) { //WidgetStateProperty digunakan untuk mengatur padding di dalaman tombol
                                 return const EdgeInsets.symmetric(
                                     horizontal: 30, vertical: 15);
                               }), backgroundColor:
-                                  WidgetStateProperty.resolveWith((states) {
+                                  WidgetStateProperty.resolveWith((states) { //WidgetStateProperty digunakan untuk mengatur warna latar belakang tombol
                                 return Colors.brown[700];
                               }), foregroundColor:
-                                  WidgetStateProperty.resolveWith((states) {
+                                  WidgetStateProperty.resolveWith((states) { //WidgetStateProperty digunakan untuk mengatur warna teks tombol
                                 return Colors.white;
                               }), shape:
-                                  WidgetStateProperty.resolveWith((states) {
-                                return RoundedRectangleBorder(
+                                  WidgetStateProperty.resolveWith((states) { //WidgetStateProperty digunakan untuk mengatur bentuk tombol
+                                return RoundedRectangleBorder( //RoundedRectangleBorder digunakan untuk mengatur bentuk tombol agar ujung kotak melengkung
                                   borderRadius: BorderRadius.circular(30),
                                 );
                               }),
